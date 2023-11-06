b7d6c0e8ad75ff0a8f7ba554871ed91ed6e4bdf8
Akuras <paulandreiscalma@gmail.com>
2023-11-06 08:47:38 +0800

    updates

--- client/src/scripts/index.js
+++ client/src/scripts/index.js
@@ -1,33 +1,72 @@
-// let test = document.querySelector(".chocolate")
+let strawberry = document.querySelector(".strawberry")
+let chocolate = document.querySelector(".chocolate")
+let cookiesNCream = document.querySelector(".cookiescream")
+let ube = document.querySelector(".ube")
 
-// function isInViewport(element) {
-//     const rect = element.getBoundingClientRect();
-//     console.log(rect)
-//     return (
-//         rect.x == 0
-//     );
-// }
+const link = document.querySelector(".made__link")
+const root = document.querySelector(":root")
 
-// if (isInViewport(test)) {
-//     console.log('true')
-// } else {
-//     console.log('false')
-// }
+let CURR_THEME
 
-// function debounce(cb, delay=2000) {
-//     return (...args) => {
-//         setTimeout(() => {
-//             cb(...args)
-//         }, delay)           
-//     }
-// }
 
-// const scrollUpdate = debounce(() => console.log('works'))
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.x == 0
+    );
+}
 
-// let cont = document.querySelector(".highlights")
+function check() {
+    if (isInViewport(strawberry)) {
+        root.style.setProperty("--currentPrimary", "#ffc0cb")
+        root.style.setProperty("--currentAccent", "#ffa8b7")
+        root.style.setProperty("--currentTextHover", "#fb6780")
+        link.style.color = "black"
+    } else if (isInViewport(chocolate)) {
+        root.style.setProperty("--currentPrimary", "#A7796D")
+        root.style.setProperty("--currentAccent", "#341104")
+        root.style.setProperty("--currentTextHover", "#64481d")
+        link.style.color = "snow"
+    } else if (isInViewport(cookiesNCream)) {
+        root.style.setProperty("--currentPrimary", "#d6c7bb")
+        root.style.setProperty("--currentAccent", "#a4928b")
+        root.style.setProperty("--currentTextHover", "#5f5349")
+        link.style.color = "black"
+    } else if (isInViewport(ube)) {
+        root.style.setProperty("--currentPrimary", "#8878c3")
+        root.style.setProperty("--currentAccent", "#7b69b6")
+        root.style.setProperty("--currentTextHover", "#11114e")
+        link.style.color = "black"
+    } else {
+        root.style.setProperty("--currentAccent", "transparent")
+    }
+}
 
-// cont.addEventListener("scroll", (e) => 
-//     {
-//         scrollUpdate()
-//     }
-// )
+// // function debounce(cb, delay=1000) {
+// //     return (...args) => {
+// //         setTimeout(() => {
+// //             cb(...args)
+// //         }, delay)           
+// //     }
+// // }
+
+// // const scrollUpdate = debounce(() => {
+// //     if (isInViewport(strawberry)) {
+// //         console.log('strawberry')
+// //     } else if (isInViewport(chocolate)) {
+// //         console.log('chocolate')
+// //     } else if (isInViewport(cookiesNCream)) {
+// //         console.log('cookies and cream')
+// //     } else if (isInViewport(ube)) {
+// //         console.log('ube')
+// //     }
+// // })
+
+let cont = document.querySelector(".highlights")
+
+cont.addEventListener("scroll", (e) => 
+    {
+        check()
+        e.stopImmediatePropagation()
+    }
+)
